c7ab986c82935a95f75bdf4e914e4ab1177142ac
ChrisLee1659 <119467134+ChrisLee1659@users.noreply.github.com>
2023-01-19 21:46:38 -0600

    added notes for what needs to be done to figure out how to get a working click on event button

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ moodButtonEl.on('click', function() {
 //add function here to return a cocktail based on the mood click button.  Not sure how to do this yet//
 console.log('return drink');
 });
+
+// Click on a button and return a result//
+// Need to create a button//
+// Identify the Button - Variable //
+// Create a click on event listener for the button//
+// Build a function that executes on click//
+// Function calls and displays a cocktail name from third party API//
+
