26e5bb5d496852ee7f0c26d55d6890863e474e96
ChrisLee1659 <119467134+ChrisLee1659@users.noreply.github.com>
2023-01-19 21:08:14 -0600

    added the starter code for a button event listener in JS

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log(fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'))
+moodButtonEl.on('click', function() {
+//add function here to return a cocktail based on the mood click button.  Not sure how to do this yet//
+console.log('return drink');
+});
